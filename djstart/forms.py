7b3311fb02a05f0b74b7e012e4e464157310ed11
Rin Kishi <rinetranger@gmail.com>
2020-12-03 22:39:03 +0900

    id-rendering

--- djstart/forms.py
+++ djstart/forms.py
@@ -1,9 +1,4 @@
 from django import forms
 
 class DjstartForm(forms.Form):
-   data = [
-       ('one','item1'),
-       ('two','item2'),
-       ('three','item3')    
-   ]
-   choice = forms.ChoiceField(label='Choice',choices=data)
+    id = forms.IntegerField(label='ID')
--- djstart/views.py
+++ djstart/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Friend
+from .forms import DjstartForm
 
 def index(request):
-    data = Friend.objects.all()
     params ={
-        'title':'djstatr',
+        'title':'djstat',
         'message':'all friends.',
-        'data':data,
+        'form':DjstartForm(),
+        'data':[],
     }
+    if(request.method =="POST"):
+        num =request.POST['id']
+        item = Friend.objects.get(id = num)
+        params['data'] = [item]
+        params['form'] = DjstartForm(request.POST)
+    else:
+        params['data']=Friend.objects.all()
 
     return render(request,'djstart/index.html',params) 
 
